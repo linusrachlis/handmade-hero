eaf7271b9fd5dc6b45761d3bca2b95f0c81190a6
Linus Rachlis <linus.rachlis@purefacts.com>
2018-03-11 17:39:03 -0400

    Painting whiteness!

---
+++ handmade/code/win32_handmade.cpp
@@ -0,0 +1,100 @@
+#include <windows.h>
+
+LRESULT CALLBACK MainWindowCallback(
+    HWND   Window,
+    UINT   Message,
+    WPARAM WParam,
+    LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+    switch (Message) {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            PatBlt(DeviceContext,
+                Paint.rcPaint.left,
+                Paint.rcPaint.top,
+                Paint.rcPaint.right - Paint.rcPaint.left,
+                Paint.rcPaint.bottom - Paint.rcPaint.top,
+                WHITENESS);
+            EndPaint(Window, &Paint);
+        } break;
+
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return(Result);
+}
+
+int CALLBACK WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR     CmdLine,
+    int       ShowCode)
+{
+    WNDCLASS WindowClass = {};
+
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    // WindowClass.hIcon;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if (RegisterClass(&WindowClass)) {
+        HWND WindowHandle = CreateWindowEx(
+            0,
+            WindowClass.lpszClassName,
+            "Handmade Hero",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            Instance,
+            0);
+
+        if (WindowHandle) {
+            for (;;) {
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0) {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            // TODO logging
+        }
+    } else {
+        // TODO logging
+    }
+}
