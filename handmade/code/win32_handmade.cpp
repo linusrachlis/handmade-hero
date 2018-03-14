9930167b7e0cc1f0e3435e1043846f12b6856da7
Linus Rachlis <linus.rachlis@purefacts.com>
2018-03-14 18:21:50 -0400

    Rendering weird but non-animating gradient!

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,6 +1,101 @@
 #include <windows.h>
+#include <stdint.h>
 
-LRESULT CALLBACK MainWindowCallback(
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running = false;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+    if (BitmapMemory)
+    {
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+    }
+
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    int BytesPerPixel = 4;
+    int BitmapMemorySize = BitmapWidth * BitmapHeight * BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+    // Pitch = byte offset from one row to the next
+    int Pitch = Width * BytesPerPixel;
+    // Create pointer to the ... first 8 bits? lower 8 bits? TODO
+    uint8_t *Row = (uint8_t *)BitmapMemory;
+
+    for (int Y = 0; Y < BitmapHeight; Y++)
+    {
+        uint8_t *Pixel = (uint8_t *)Row;
+
+        for (int X = 0; X < BitmapWidth; X++)
+        {
+            // Set blue byte
+            // *Pixel = 0;
+            *Pixel = (uint8_t)X;
+            // Advance pointer one byte
+            Pixel++;
+
+            // Set green byte
+            // *Pixel = 0;
+            *Pixel = (uint8_t)Y;
+            Pixel++;
+
+            // Set red byte
+            *Pixel = Y/255 * X;
+            Pixel++;
+
+            // Set padding byte (why wasn't this first? TODO)
+            *Pixel = 0;
+            Pixel++;
+        }
+
+        // Advance row pointer by number of bytes per row
+        Row += Pitch;
+    }
+
+	OutputDebugStringA("Finished bitmap");
+}
+
+internal void Win32UpdateWindow(
+    HDC DeviceContext,
+    RECT *WindowRect,
+    int X,
+    int Y,
+    int Width,
+    int Height)
+{
+    // TODO for now, ignoring the desired repainting rectangle, and just repainting the
+    // whole window every time. If we stick with this way, then stop passing X,Y,Width,Height.
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
+    StretchDIBits(DeviceContext,
+        /*
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        */
+        0, 0, BitmapWidth, BitmapHeight,
+        0, 0, WindowWidth, WindowHeight,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS, SRCCOPY);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(
     HWND   Window,
     UINT   Message,
     WPARAM WParam,
@@ -8,20 +103,22 @@ LRESULT CALLBACK MainWindowCallback(
 {
     LRESULT Result = 0;
 
-    switch (Message) {
+    switch (Message)
+    {
         case WM_SIZE:
         {
             OutputDebugStringA("WM_SIZE\n");
-        } break;
-
-        case WM_DESTROY:
-        {
-            OutputDebugStringA("WM_DESTROY\n");
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
         } break;
 
         case WM_CLOSE:
         {
             OutputDebugStringA("WM_CLOSE\n");
+            Running = false;
         } break;
 
         case WM_ACTIVATEAPP:
@@ -29,18 +126,25 @@ LRESULT CALLBACK MainWindowCallback(
             OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
 
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+            Running = false;
+        } break;
+
         case WM_PAINT:
         {
-            static DWORD Operation;
-            Operation = Operation == WHITENESS ? BLACKNESS : WHITENESS;
             PAINTSTRUCT Paint;
             HDC DeviceContext = BeginPaint(Window, &Paint);
-            PatBlt(DeviceContext,
-                Paint.rcPaint.left,
-                Paint.rcPaint.top,
-                Paint.rcPaint.right - Paint.rcPaint.left,
-                Paint.rcPaint.bottom - Paint.rcPaint.top,
-                Operation);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break;
 
@@ -62,7 +166,7 @@ int CALLBACK WinMain(
     WNDCLASS WindowClass = {};
 
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     // WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
@@ -83,7 +187,8 @@ int CALLBACK WinMain(
             0);
 
         if (WindowHandle) {
-            for (;;) {
+            Running = true;
+            while (Running) {
                 MSG Message;
                 BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
                 if (MessageResult > 0) {
@@ -99,4 +204,6 @@ int CALLBACK WinMain(
     } else {
         // TODO logging
     }
+
+    return(0);
 }
