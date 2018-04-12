8c803c98eaac2da78da510ae0938dc4a6f1f3471
Linus Rachlis <linus@rachlis.net>
2018-04-11 23:37:18 -0400

    Detect paddle collisions; detect victory

--- handmade/code/handmade.cpp
+++ handmade/code/handmade.cpp
@@ -1,3 +1,10 @@
+/*
+TODO
+- controls
+- paddle hit angling
+- AI?
+*/
+
 struct game_offscreen_buffer
 {
     void *Memory;
@@ -39,9 +46,11 @@ struct puck
 
 global_variable int GameWidth;
 global_variable int GameHeight;
+global_variable bool Victory = false;
 
 // Pixel structure in register: xx RR GG BB
 global_variable const uint32_t White = (255 << 16) | (255 << 8) | 255;
+global_variable const uint32_t Red = (255 << 16);
 global_variable const uint32_t Black = 0;
 
 global_variable puck Puck;
@@ -55,14 +64,12 @@ internal void GameSetup(int Width, int Height)
     GameWidth = Width;
     GameHeight = Height;
 
-    // TODO: register boxes to be rendered
-
     Puck.Box.Top = 100;
     Puck.Box.Left = 100;
     Puck.Box.Width = 50;
     Puck.Box.Height = 50;
-    Puck.Velocity.X = 3;
-    Puck.Velocity.Y = 3;
+    Puck.Velocity.X = 5;
+    Puck.Velocity.Y = 5;
 
     LeftPaddle.Top = 0;
     LeftPaddle.Left = 0;
@@ -74,6 +81,7 @@ internal void GameSetup(int Width, int Height)
     RightPaddle.Height = 200;
     RightPaddle.Left = GameWidth - RightPaddle.Width;
 
+    // Register boxes for the renderer
     BoxesToRender[0] = &Puck.Box;
     BoxesToRender[1] = &LeftPaddle;
     BoxesToRender[2] = &RightPaddle;
@@ -83,64 +91,84 @@ internal void
 GameUpdateAndRender(game_offscreen_buffer *Buffer)
 {
     //
-    // NOTE: Update
+    // NOTE: Update game state, unless victory has been achieved.
     //
 
-    if (Puck.Velocity.X > 0) {
-        // Going right
-        if ((GameWidth - Puck.Box.Right()) < Puck.Velocity.X)
-        {
-            // Bounce
-            Puck.Velocity.X *= -1;
-        }
-        else
-        {
-            Puck.Box.Left += Puck.Velocity.X;
-        }
-    }
-    else if (Puck.Velocity.X < 0)
+    if (!Victory)
     {
-        // Going left
-        if (Puck.Box.Left < -Puck.Velocity.X)
-        {
-            // Bounce
-            Puck.Velocity.X *= -1;
+        // X collision detection is only about paddles, and interacts with
+        // victory conditions.
+        if (Puck.Velocity.X > 0) {
+            // Going right
+            if (((RightPaddle.Left - Puck.Box.Right()) < Puck.Velocity.X) &&
+                (Puck.Box.Bottom() > RightPaddle.Top) &&
+                (Puck.Box.Top < RightPaddle.Bottom()))
+            {
+                // If the paddle is there to block the puck, bounce
+                Puck.Velocity.X *= -1;
+            }
+            else if (Puck.Box.Right() >= GameWidth)
+            {
+                Victory = true;
+            }
+            else
+            {
+                Puck.Box.Left += Puck.Velocity.X;
+            }
         }
-        else
+        else if (Puck.Velocity.X < 0)
         {
-            Puck.Box.Left += Puck.Velocity.X;
+            // Going left
+            if (((Puck.Box.Left - LeftPaddle.Right()) < Puck.Velocity.X) &&
+                (Puck.Box.Bottom() > LeftPaddle.Top) &&
+                (Puck.Box.Top < LeftPaddle.Bottom()))
+            {
+                // If the paddle is there to block the puck, bounce
+                Puck.Velocity.X *= -1;
+            }
+            else if (Puck.Box.Left <= 0)
+            {
+                Victory = true;
+            }
+            else
+            {
+                Puck.Box.Left += Puck.Velocity.X;
+            }
         }
-    }
 
-    if (Puck.Velocity.Y > 0) {
-        // Going down
-        if ((GameHeight - Puck.Box.Bottom()) < Puck.Velocity.Y)
-        {
-            // Bounce
-            Puck.Velocity.Y *= -1;
+        // Y collision detection is only about floor and ceiling.
+        if (Puck.Velocity.Y > 0) {
+            // Going down
+            if ((GameHeight - Puck.Box.Bottom()) < Puck.Velocity.Y)
+            {
+                // Bounce
+                Puck.Velocity.Y *= -1;
+            }
+            else
+            {
+                Puck.Box.Top += Puck.Velocity.Y;
+            }
         }
-        else
+        else if (Puck.Velocity.Y < 0)
         {
-            Puck.Box.Top += Puck.Velocity.Y;
-        }
-    }
-    else if (Puck.Velocity.Y < 0)
-    {
-        // Going up
-        if (Puck.Box.Top < -Puck.Velocity.Y)
-        {
-            // Bounce
-            Puck.Velocity.Y *= -1;
-        }
-        else
-        {
-            Puck.Box.Top += Puck.Velocity.Y;
+            // Going up
+            if (Puck.Box.Top < -Puck.Velocity.Y)
+            {
+                // Bounce
+                Puck.Velocity.Y *= -1;
+            }
+            else
+            {
+                Puck.Box.Top += Puck.Velocity.Y;
+            }
         }
     }
 
     //
     // NOTE: Render
     //
+
+    uint32_t BoxColour = Victory ? Red : White;
 
     // Note: Row has to be a 1-byte pointer because we use the Pitch to advance it, and
     // Pitch is a number of bytes.
@@ -165,7 +193,7 @@ GameUpdateAndRender(game_offscreen_buffer *Buffer)
                 }
             }
 
-            *Pixel = PixelIsOccupied ? White : Black;
+            *Pixel = PixelIsOccupied ? BoxColour : Black;
 
             // Advance to write next pixel
             Pixel++;
