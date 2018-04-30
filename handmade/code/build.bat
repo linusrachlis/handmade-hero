ad1e830d755b9a20e1c79f15fcf19762e7d3d99a
Linus Rachlis <linus@rachlis.net>
2018-04-30 13:08:57 -0400

    Correct build.bat comment

--- handmade/code/build.bat
+++ handmade/code/build.bat
@@ -2,7 +2,7 @@
 pushd ..\..\build
 rem disable warning 4100: unreferenced formal parameter
 rem disable warning 4201: nameless struct/union
-rem SOMETIMES disable warning 4201: local var initialized but not used
+rem SOMETIMES disable warning 4189: local var initialized but not used
 cl -MT -Gm- -nologo -Oi -Od -GR- -FC -Zi -WX -W4 ^
     -wd4100 -wd4201 ^
     -wd4189 ^
