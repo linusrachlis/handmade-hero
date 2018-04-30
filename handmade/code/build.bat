a47830fad4f26d31fbacb8c64fec49d9ee4079f9
Linus Rachlis <linus@rachlis.net>
2018-04-30 11:24:18 -0400

    Fail on compiler warnings, disable some warnings
    + some other compiler switches
    
    Day 016

--- handmade/code/build.bat
+++ handmade/code/build.bat
@@ -1,5 +1,11 @@
 @echo off
-mkdir ..\..\build
 pushd ..\..\build
-cl /FC -Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
+rem disable warning 4100: unreferenced formal parameter
+rem disable warning 4201: nameless struct/union
+rem SOMETIMES disable warning 4201: local var initialized but not used
+cl -MT -Gm- -nologo -Oi -Od -GR- -FC -Zi -WX -W4 ^
+    -wd4100 -wd4201 ^
+    -wd4189 ^
+    -DHANDMADE_SLOW=1 ^
+    ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
